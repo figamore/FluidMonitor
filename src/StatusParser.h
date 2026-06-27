fb5a5e6dc1b45a1f9a4e2010c485af1f40ec6f09
figamore <90107339+figamore@users.noreply.github.com>
2026-06-27 07:20:06 -0400

    Replace EspNowLink with FluidNC-EspNow-Client

--- src/StatusParser.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 Figamore (https://github.com/figamore)
-// SPDX-License-Identifier: GPL-3.0-only
-
-#pragma once
-
-#include <stddef.h>
-#include <stdint.h>
-
-void consumeSecureBytes(const uint8_t* data, size_t len);
